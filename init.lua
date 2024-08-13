7d39a4207988c92eb326be18efbf750b178a18b9
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-08-13 01:22:43 -0400

    Remove auto session, too much conflicts

--- init.lua
+++ init.lua
@@ -383,18 +383,6 @@ require('lazy').setup({
   {
     'tpope/vim-fugitive',
   },
-  {
-    'rmagatti/auto-session',
-    lazy = false,
-    dependencies = {
-      'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
-    },
-    config = function()
-      require('auto-session').setup {
-        auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-      }
-    end,
-  },
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
